42a422defdb0d45ca7a116189faae0ae741a322d
Klif <sang.wicklife@student.moringaschool.com>
2023-12-13 00:42:08 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+// Assuming Hq is at 42 st
+function distanceFromHqInBlocks(location) {
+    if (location>42) {
+        return location-42;
+    } else {
+        return 42-location;
+    }
+}
+
+function distanceFromHqInFeet(location) {
+    // 1 block equals 264 ft
+    if (location>42) {
+        return (location-42)*264;
+    } else {
+        return (42-location)*264;
+    }
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.max(start,destination)- Math.min(start,destination))*264;
+}
+
+function  calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+        return 0;
+    } else if (distance <= 2000){
+        return (distance-400) * 2/100;
+    } else if(distance <= 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
